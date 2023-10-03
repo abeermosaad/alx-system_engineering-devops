2b439d8eaf8900cec01c10436693afb6ddd9c955
abeermosaad <abeermosaad69@gmail.com>
2023-10-03 05:34:37 +0300

    Automate the task with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,6 @@
+file_line { ' creating a custom HTTP header response':
+  path  => '/etc/nginx/sites-available/default',
+  line  => '		add_header X-Served-By $hostname;',
+	after  => '^\s*server\s*\{',
+  ensure => present,
+}
