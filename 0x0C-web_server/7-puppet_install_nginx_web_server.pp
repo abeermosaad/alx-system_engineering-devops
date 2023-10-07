bc3d32290599956bcc88dcaa20e874b61bb1a42e
abeermosaad <abeermosaad69@gmail.com>
2023-10-08 01:56:36 +0300

    Fix puppet-lint

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 exec { 'update':
-	path     => '/usr/bin',
-	command  => 'sudo apt-get update',
+    path    => '/usr/bin',
+    command => 'sudo apt-get update',
 }
 
 exec { 'install_nginx':
@@ -10,32 +10,31 @@ exec { 'install_nginx':
 }
 
 exec { 'allow':
-	path     => '/usr/bin',
-	command  => "sudo ufw allow 'Nginx HTTP'",
-	provider => 'shell',
-	unless  => "sudo ufw status | grep -q 'Nginx HTTP'",
+    path     => '/usr/bin',
+    command  => "sudo ufw allow 'Nginx HTTP'",
+    provider => 'shell',
+    unless   => "sudo ufw status | grep -q 'Nginx HTTP'",
 }
 
 exec { 'mk dir':
-    path     => '/usr/bin',
-    command  => "sudo mkdir -p /var/www/abeermosaad.tech/html",
-    returns  => [0, 1],
+    path    => '/usr/bin',
+    command => 'sudo mkdir -p /var/www/abeermosaad.tech/html',
+    returns => [0, 1],
 }
 
 file { '/var/www/html/index.nginx-debian.html':
-	ensure => 'present',
-	content => "Hello World!\n"
+    ensure  => 'present',
+    content => "Hello World!\n"
 }
 
 file { '/var/www/abeermosaad.tech/html/custom_404.html':
-	ensure => 'present',
-	content => "Ceci n'est pas une page\n"
+    ensure  => 'present',
+    content => "Ceci n'est pas une page\n"
 }
 
 file { '/etc/nginx/sites-available/default':
-	ensure => 'present',
-	content => "server {
-        add_header X-Served-By $hostname;
+    ensure  => 'present',
+    content => "server {
         listen 80 default_server;
         listen [::]:80 default_server;
 
@@ -44,6 +43,8 @@ file { '/etc/nginx/sites-available/default':
         index index.html index.htm index.nginx-debian.html;
 
         server_name abeermosaad.tech;
+        add_header X-Served-By ${{hostname}};
+
         location /hbnb_static/ {
                 alias /data/web_static/current/;
         }
@@ -61,7 +62,7 @@ file { '/etc/nginx/sites-available/default':
         location / {
                 root /var/www/abeermosaad.tech/html;
                 index index.html;
-                try_files $uri $uri/ =404;
+                try_files ${{uri}} ${{uri}}/ =404;
         }
 }
 "
