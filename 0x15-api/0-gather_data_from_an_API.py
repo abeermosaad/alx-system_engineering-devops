a84b39d9a204b5e77c3027eb7de1528efcdbc57d
abeermosaad <abeermosaad69@gmail.com>
2023-10-25 01:13:19 +0300

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+url = f"https://jsonplaceholder.typicode.com/users/{int(sys.argv[1])}/"
+response = requests.get(url)
+
+url2 = f"https://jsonplaceholder.typicode.com/todos?userId={int(sys.argv[1])}"
+response2 = requests.get(url2)
+
+employee = response.json()
+todos = response2.json()
+
+complete = 0
+total = len(todos)
+
+for i in range(total):
+    if todos[i].get('completed'):
+        complete += 1
+
+print(
+    f'Employee {employee.get("name")} is done with tasks({complete}/{total}):')
+
+for i in range(total):
+    if todos[i].get('completed'):
+        print(f'\t {todos[i].get("title")}')
