fd6039a3a9ff28ced37de7acf14021af12c9a6e9
abeermosaad <abeermosaad69@gmail.com>
2023-11-07 18:59:25 +0200

    Change the logic of steps

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,11 +8,11 @@ def top_ten(subreddit):
 
     url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
     res = requests.get(url, headers={'User-Agent': 'abeer'})
-    top = res.json()
 
     if res.status_code != 200:
         print(None)
         return
 
+    top = res.json()
     for post in top['data']['children']:
         print(post['data']['title'])
