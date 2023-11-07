31e71658b5088a49401d32bb1516e9766de934af
abeermosaad <abeermosaad69@gmail.com>
2023-11-07 18:54:52 +0200

    Top ten posts API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""request top ten posts of a subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """returns the top ten posts of a subreddit"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
+    res = requests.get(url, headers={'User-Agent': 'abeer'})
+    top = res.json()
+
+    if res.status_code != 200:
+        print(None)
+        return
+
+    for post in top['data']['children']:
+        print(post['data']['title'])
