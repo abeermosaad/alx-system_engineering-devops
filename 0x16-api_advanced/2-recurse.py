7a401e9c58fba01aa5ed11d75f50c90dfe5a25d9
abeermosaad <abeermosaad69@gmail.com>
2023-11-08 16:18:01 +0200

    Pagination for separating pages

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""requests module"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ returns a list containing the titles of all hot articles for a given subreddit """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    res = requests.get(url, headers={'User-Agent': 'abeer'})
+
+    if res.status_code != 200:
+        return
+
+    hot = res.json()
+    if not hot['data']['after']:
+        return hot_list
+
+    after = hot['data']['after']
+
+    for article in hot['data']['children']:
+        hot_list.append(article['data']['title'])
+
+    return (recurse(subreddit, hot_list, after))
