be2ab1ba0496fee206d13bdcaea163f5f252f0a4
abeermosaad <abeermosaad69@gmail.com>
2023-11-16 22:54:14 +0200

    Fix failed requests-1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,11 @@
 # Fix stack so that we get to 0 failed requests
 
 exce {'sed'
+    provider => 'shell',
     command => 'sudo sed -i 's/ULIMIT='-n 15'/ULIMIT='-n 4096'/' /etc/default/nginx'
+    before => Exec['restart']
 }
 exce {'restart'
+    provider => 'shell',
     command => 'sudo service nginx restart'
 }
